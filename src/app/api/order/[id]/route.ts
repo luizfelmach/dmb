f102389891147e1766f13a88b4acb941329083b4
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-07-29 09:34:54 -0300

    format default

--- src/app/api/order/[id]/route.ts
+++ src/app/api/order/[id]/route.ts
@@ -1,22 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/db/prisma";
 
-export async function GET(request : NextRequest, {params}: {params: {id: string}}) {
-    try {
-        const { id } = params;
-        const order = await prisma.order.findUnique({
-            where: {
-                id
-            },
-            include: {
-                services: true
-            }
-        })
-        return NextResponse.json(order)
-    } catch (error: any) {
-        return NextResponse.json({
-            status: "error",
-            message: error.message
-        })
-    }
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
+    const order = await prisma.order.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        services: true,
+      },
+    });
+    return NextResponse.json(order);
+  } catch (error: any) {
+    return NextResponse.json({
+      status: "error",
+      message: error.message,
+    });
+  }
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import localFont from 'next/font/local';
+import "./globals.css";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 
-const inter = localFont({ src: '../assets/fonts/inter/Inter-Regular.ttf' })
-
+const inter = localFont({ src: "../assets/fonts/inter/Inter-Regular.ttf" });
 
 export const metadata: Metadata = {
-  title: 'Darlene Machado Buffet',
-  description: 'Casamentos, Aniversários, Eventos Corporativos, Coffe Break, Buffet Infantil, Churrasco & Aluguel de utensílios',
-}
+  title: "Darlene Machado Buffet",
+  description:
+    "Casamentos, Aniversários, Eventos Corporativos, Coffe Break, Buffet Infantil, Churrasco & Aluguel de utensílios",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
