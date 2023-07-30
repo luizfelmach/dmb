d914cec2870824d8c0cde9c5cfe3af3c29dd95de
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-07-30 17:00:32 -0300

    add CheckButton

---
+++ src/components/CheckButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Box } from "@chakra-ui/react";
+
+export function CheckButton(props: any) {
+  const { checked, children } = props;
+
+  return (
+    <Box
+      border={"1px"}
+      borderColor={!checked ? "whiteAlpha.200" : "purple.100"}
+      bgColor={!checked ? "whiteAlpha.200" : "whiteAlpha.300"}
+      padding={"10px"}
+      borderRadius={"lg"}
+      height={"10"}
+      flexGrow={1}
+      _hover={
+        !checked
+          ? {
+              bgColor: "whiteAlpha.100",
+              cursor: "pointer",
+            }
+          : {
+              cursor: "pointer",
+            }
+      }
+      {...props}
+    >
+      {children}
+    </Box>
+  );
+}
